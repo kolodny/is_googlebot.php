f67564cacdd3291a967cb4f2ea312ff16e07f892
Moshe Kolodny <moshe@moshe-linux.(none)>
2013-06-07 09:50:23 -0400

    added option to skip user agent check

--- is_googlebot.php
+++ is_googlebot.php
@@ -1,5 +1,5 @@
-function is_googlebot($ip) {
-    if (strpos($_SERVER['HTTP_USER_AGENT'], 'Google') !== false) {
+function is_googlebot($ip, $check_user_agent = true) {
+    if (!$check_user_agent || strpos($_SERVER['HTTP_USER_AGENT'], 'Google') !== false) {
         $host = gethostbyaddr($ip);
         if (substr($host, -14) == '.googlebot.com') {
             $host_ips = gethostbynamel($host);
